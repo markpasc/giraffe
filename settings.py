4777dd20cd010a01f00d0994abd187caa88d1e5a
Mark Paschal <markpasc@markpasc.org>
2009-06-14 22:17:13 -0700

    Try to fix GAE template loader weirdness

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
 # Django settings for giraffe project.
 
+import os
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -63,11 +65,13 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'urls'
+ROOT_PATH = os.path.dirname(__file__)
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(ROOT_PATH, 'templates'),
 )
 
 INSTALLED_APPS = (
