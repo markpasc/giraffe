59a4bfe1c76cb9cb48c55c2b7c6146b4ef8c2c4d
Mark Paschal <markpasc@markpasc.org>
2009-06-24 15:37:55 -0700

    Configurate log levels per settings

--- library/models/__init__.py
+++ library/models/__init__.py
@@ -1,3 +1,14 @@
 from library.models.base import *
 from library.models.auth import *
 from library.models.stuff import *
+
+
+import logging
+
+from django.conf import settings
+
+for zone, level in getattr(settings, 'LOG_LEVELS', {}).items():
+    log = logging.getLogger(zone)
+    log.setLevel(level)
+    logging.info('Set log %s to level %s', zone,
+        logging.getLevelName(level))
