352790c5bc6760dc498a97ec7138db94bf529483
Mark Paschal <markpasc@markpasc.org>
2009-09-09 14:19:15 -0700

    spacing

--- static/jquery.elastic.js
+++ static/jquery.elastic.js
@@ -21,14 +21,14 @@
 				'fontFamily',
 				'width',
 				'fontWeight'];
-			
+
 			return this.each( function() {
-				
+
 				// Elastic only works on textareas
 				if ( this.type != 'textarea' ) {
 					return false;
 				}
-				
+
 				var $textarea	=	jQuery(this),
 					$twin		=	jQuery('<div />').css({'position': 'absolute','display':'none'}),
 					lineHeight	=	parseInt($textarea.css('line-height'),10) || parseInt($textarea.css('font-size'),'10'),
@@ -36,41 +36,40 @@
 					maxheight	=	parseInt($textarea.css('max-height'),10) || Number.MAX_VALUE,
 					goalheight	=	0,
 					i 			=	0;
-					
+
 				// Append the twin to the DOM
 				// We are going to meassure the height of this, not the textarea.
 				$twin.appendTo($textarea.parent());
-				
+
 				// Copy the essential styles (mimics) from the textarea to the twin
 				$.each(mimics, function (i, mimic) {
 				    $twin.css(mimic, $textarea.css(mimic));
 				});
-				
+
 				// Sets a given height and overflow state on the textarea
 				function setHeightAndOverflow(height, overflow){
 					curratedHeight = Math.floor(parseInt(height,10));
 					if($textarea.height() != curratedHeight){
 						$textarea.css({'height': curratedHeight + 'px','overflow':overflow});
-						
+
 					}
 				}
-				
-				
+
 				// This function will update the height of the textarea if necessary 
 				function update() {
-					
+
 					// Get curated content from the textarea.
 					var textareaContent = $textarea.val().replace(/<|>/g, ' ').replace(/\n/g, '<br />').replace(/&/g,"&amp;");
 					var twinContent = $twin.html();
-					
+
 					if(textareaContent+'&nbsp;' != twinContent){
-					
+
 						// Add an extra white space so new rows are added when you are at the end of a row.
 						$twin.html(textareaContent+'&nbsp;');
-						
+
 						// Change textarea height if twin plus the height of one line differs more than 3 pixel from textarea height
 						if(Math.abs($twin.height()+lineHeight - $textarea.height()) > 3){
-							
+
 							var goalheight = $twin.height()+lineHeight;
 							if(goalheight >= maxheight) {
 								setHeightAndOverflow(maxheight,'auto');
@@ -79,24 +78,24 @@
 							} else {
 								setHeightAndOverflow(goalheight,'hidden');
 							}
-							
+
 						}
-						
+
 					}
-					
+
 				}
-				
+
 				// Hide scrollbars
 				$textarea.css({'overflow':'hidden'});
-				
+
 				// Update textarea size on keyup
 				$textarea.keyup(function(){ update(); });
-				
+
 				// Run update once when elastic is initialized
 				update();
-				
+
 			});
-			
+
         } 
     }); 
 })(jQuery);
