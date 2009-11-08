6725497a889014f4ecd86b9a8e10f5286009d47e
Martin Atkins <mart@degeneration.co.uk>
2009-11-07 21:42:21 -0800

    Add a simple module that can query the Google Social Graph API so that we might in future use it to import account information.

---
+++ giraffe/socialgraphapi.py
@@ -0,0 +1,60 @@
+
+"""
+Functions for interacting with Google's Social Graph API.
+"""
+
+import urllib2
+import urllib
+import simplejson as json
+
+SGAPI_LOOKUP = "http://socialgraph.apis.google.com/lookup"
+
+class SocialGraphNode:
+
+    nodes = None
+    uri = None
+
+    def __init__(self, nodes, uri):
+        self.nodes = nodes
+        self.uri = uri
+
+    def json_obj(self):
+        return self.nodes[self.uri]
+
+    def attributes(self):
+        return self.json_obj()["attributes"]
+
+    def claimed_nodes(self):
+        return map(lambda uri : SocialGraphNode(self.nodes, uri), self.json_obj()["claimed_nodes"])
+
+    def __unicode__(self):
+        return self.uri
+
+def lookup_node(uri):
+    nodes = lookup_nodes([uri])
+    return nodes[uri]
+
+def lookup_nodes(uris):
+    ret = {}
+
+    params = [
+        ("fme", "1"),
+        ("pretty", "0"),
+        ("sgn", "1"),
+        ("q", ",".join(uris))
+    ]
+    qs = urllib.urlencode(params)
+    url = "?".join([SGAPI_LOOKUP, qs])
+
+    result = urllib2.urlopen(url)
+    struct = json.load(result)
+
+    nodes = struct["nodes"]
+    canonical_mapping = struct["canonical_mapping"]
+
+    for uri in uris:
+        canon_uri = canonical_mapping[uri]
+        ret[uri] = SocialGraphNode(nodes, canon_uri)
+
+    return ret
+
